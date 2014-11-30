cba3d6046e853c48ee4c3a0511b7faec52b5dafa
Jdsemler <jdsemler@gmail.com>
2014-11-30 08:22:59 -0600

    Lab 15
    It's not perfect but I really need the grade.
    
    Next class could you please help me with the collision detection and
    making the target move? Those two components are really big in our
    current project and I'm really struggling on trying to figure out how to
    do them.

--- 100pt.py
+++ 100pt.py
@@ -18,7 +18,7 @@ targety1 = 20
 targetx2 = 280
 targety2 = 80
 target = drawpad.create_rectangle(targetx1,targety1,targetx2,targety2, fill="blue")
-player = drawpad.create_rectangle(240,240,260,260, fill="pink")
+oval = drawpad.create_rectangle(240,240,260,260, fill="pink")
 direction = 4
 
 
@@ -30,18 +30,60 @@ class MyApp:
 		self.myContainer1 = Frame(parent)
 		self.myContainer1.pack()
 		
-		self.up = Button(self.myContainer1)
-		self.up.configure(text="Up", background= "green")
-		self.up.grid(row=0,column=0)
-					
-		# "Bind" an action to the first button												
-		self.up.bind("<Button-1>", self.moveUp)
-                
-		  
-		# This creates the drawpad - no need to change this 
+		self.button1 = Button(self.myContainer1)
+ 		self.button1.configure(text="Left", background= "green")
+ 		self.button1.grid(row=0,column=1)
+		self.button1.bind("<Button-1>", self.button1Click)
+ 	        # Add a second button!
+						
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="up", background= "yellow")
+		self.button2.grid(row=0,column=2)		
+		self.button2.bind("<Button-1>", self.button2Click)
+			
+		self.button3 = Button(self.myContainer1)
+		self.button3.configure(text="down", background= "red")
+		self.button3.grid(row=0,column=3)	
+		self.button3.bind("<Button-1>", self.button3Click)
+		
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="right", background= "light blue")
+		self.button4.grid(row=0,column=4)		
+		self.button4.bind("<Button-1>", self.button4Click)
+		
+		#Creates the drawpad
 		drawpad.pack()
-		self.animate()
-
+		self.animate
+					
+		# BINDING THE BUTTONS												
+	def button1Click(self, event):   
+		# Make the oval move to the left!
+                # "global" makes sure that we can access our oval and our drawpad
+		drawpad.move(oval,-20,0)
+		global oval
+		global drawpad
+			
+ 	def button2Click(self, event):   
+ 		# Make the oval move up!
+                 # "global" makes sure that we can access our oval and our drawpad
+		drawpad.move(oval,0,-20)
+ 		global oval
+ 		global drawpad
+ 		
+        def button3Click(self, event):   
+ 		# Make the oval move down!
+                 # "global" makes sure that we can access our oval and our drawpad
+		drawpad.move(oval,0,20)
+ 		global oval
+ 		global drawpad
+ 		
+ 	def button4Click(self, event):   
+ 		# Make the oval move to the right!
+                 # "global" makes sure that we can access our oval and our drawpad
+		drawpad.move(oval,20,0)
+ 		global oval
+ 		global drawpad
+ 		
 		
 	def moveUp(self, event):   
 		global player
@@ -56,7 +98,6 @@ class MyApp:
 	    global target
 	    global direction
 	    tx1,ty1,tx2,ty2 = drawpad.coords(target)
-	    
 	    # Insert the code here to make the target move, bouncing on the edges    
 	        
 	        
@@ -74,6 +115,19 @@ class MyApp:
                 global player
                 # Get the co-ordinates of our player AND our target
                 # using x1,y1,x2,y2 = drawpad.coords(object)
+                
+                # Get the x and y co-ordinates of the circle
+                x1, y1, x2, y2 = drawpad.coords(target)
+                if x2 > drawpad.winfo_width(): 
+                    dir1 = -6
+                elif x1 < 0:
+                    dir1 = 6
+                #Move our oval object by the value of direction
+                drawpad.move(target,dir1,0)
+                
+                # Wait for 1 millisecond, then recursively call our animate function
+                drawpad.after(5, animate2)
+            
 
                 # Do your if statement - remember to return True if successful!                
 		
